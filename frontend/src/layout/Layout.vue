08c91fc9fbd189004dc91d97b033afba34ed2068
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-07-12 15:44:32 +0900

    [update]LayoutにFooter適用

--- frontend/src/layout/Layout.vue
+++ frontend/src/layout/Layout.vue
@@ -4,20 +4,19 @@
     <main>
       <slot></slot>
     </main>
-
-    <footer>
-      <!-- 푸터 내용 -->
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 
 @Component({
   components: {
     Header,
+    Footer,
   },
 })
 export default class Layout extends Vue {}
