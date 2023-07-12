0523aab9eee84db34ac98108f192d5ae25b61706
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-07-12 15:36:23 +0900

    [add]Layout.vue

---
+++ frontend/src/layout/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <!-- 푸터 내용 -->
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "../components/Header.vue";
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class Layout extends Vue {}
+</script>
+
+<style scoped></style>
