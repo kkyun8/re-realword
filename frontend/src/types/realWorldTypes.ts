072eeaaaf09968cc7ffff28950c2e8ed46304bbb
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-07-16 21:14:48 +0900

    [add]type追加

---
+++ frontend/src/types/realWorldTypes.ts
@@ -0,0 +1,29 @@
+export interface RealWorldTypes {
+  articles: Article[];
+  articlesCount: number;
+}
+
+export interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: Author;
+}
+
+export interface Author {
+  username: string;
+  bio: null;
+  image: string;
+  following: boolean;
+}
+
+export interface ArticlesResponse {
+  articles: Article[];
+  articlesCount: number;
+}
