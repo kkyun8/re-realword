b6d41f07126346b59b0e62d0291b3f75017fa296
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-07-24 23:24:51 +0900

    [update]auth系の処理でユーザーを表示＆トークン確認処理追加

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-light">
     <div class="container">
       <router-link class="navbar-brand" to="/">conduit</router-link>
-      <ul class="nav navbar-nav pull-xs-right">
+      <ul v-show="!isAuth" class="nav navbar-nav pull-xs-right">
         <li class="nav-item">
           <!-- Add "active" class when you're on that page" -->
           <router-link class="nav-link active" to="/">Home</router-link>
@@ -14,15 +14,50 @@
           <router-link class="nav-link" to="/register">Sign up</router-link>
         </li>
       </ul>
+      <ul v-show="isAuth" class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <a class="nav-link active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/editor">
+            <i class="ion-compose"></i>&nbsp;New Article
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/settings">
+            <i class="ion-gear-a"></i>&nbsp;Settings
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/profile/eric-simons">
+            <img :src="user.image" class="user-pic" />
+            {{ user.username }}
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import AuthStore from "@/store/modules/auth";
 
 @Component
-export default class Header extends Vue {}
+export default class Header extends Vue {
+  get isAuth() {
+    return AuthStore.isAuthenticated;
+  }
+
+  get user() {
+    return AuthStore.user;
+  }
+
+  async created() {
+    await AuthStore.checkAuthStatus();
+  }
+}
 </script>
 
 <style scoped></style>
